accb30cacefa678984c018367be6d8bda9d16c0d
Zian Wang <841048736@qq.com>
2024-04-08 17:31:25 +0800

    Add volatile demo for stoping a thread

---
+++ src/CoreKnowledge/StopThread/VolatileDemo/NotStopVolatile.java
@@ -0,0 +1,73 @@
+package CoreKnowledge.StopThread.VolatileDemo;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+/**
+ *   演示用 volatile 的局限性：
+ *   陷入阻塞时，volatile 是无法停止线程的。
+ *   此例中，生产者的生产速度很快，消费者消费速度慢，
+ *   所以阻塞队列满了以后，生产者会阻塞，等待消费者进一步消费。
+ */
+public class NotStopVolatile {
+	public static void main(String[] args) throws InterruptedException{
+		ArrayBlockingQueue<Integer> storage = new ArrayBlockingQueue<>(10);
+		Producer p = new Producer(storage);
+		Thread t = new Thread(p);
+		// 生产者开始生产，塞满阻塞队列后阻塞后续生产。
+		t.start();
+		Thread.sleep(1000);
+
+		// 消费者开始消费，直至消费结束。
+		Consumer c = new Consumer(storage);
+		System.out.println(c.takeNumbers(10).toString());
+
+		// 关闭生产者，但实际情况：程序仍在运行。
+		p.canceled=true;
+		System.out.println(p.canceled);
+	}
+}
+
+
+class Producer implements Runnable{
+	public volatile boolean canceled = false;
+	public BlockingQueue<Integer> bq;
+	public Producer(BlockingQueue<Integer> bq)  { this.bq = bq; }
+	@Override
+	public void run() {
+		int num = 0;
+		try {
+			while (num <= 100000 && !canceled) {
+				if (num % 100 == 0) {
+					// BlockingQueue 中的 put() 和 take() 方法 可以在阻塞中响应中断，并抛出异常。
+					// 但是使用 volatile 标志位的话，程序会一直阻塞在这里，无法检查 canceled 变量，也无法退出。
+					bq.put(num);
+					System.out.println(num + "是100的倍数,被放到仓库中了。");
+				}
+				num++;
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} finally {
+			System.out.println("生产者结束运行");
+		}
+	}
+}
+
+class Consumer {
+	public BlockingQueue<Integer> bq;
+	public Consumer(BlockingQueue<Integer> bq) { this.bq = bq; }
+	public List<Integer> takeNumbers (int num) throws InterruptedException{
+		LinkedList<Integer> res = new LinkedList<>();
+		for(int i=0; i<num; i++){
+			res.addLast(bq.take());
+			System.out.println(res.peekLast() + "被消费了");
+			// 保证消费完成后，生产者继续生产，保持队列阻塞。
+			Thread.sleep(100);
+		}
+		System.out.println("消费者完毕！");
+		return res;
+	}
+}
---
+++ src/CoreKnowledge/StopThread/VolatileDemo/StopThreadVolatile.java
@@ -0,0 +1,39 @@
+package CoreKnowledge.StopThread.VolatileDemo;
+
+/**
+ *     演示用 volatile 的局限：part1 看似可行
+ */
+public class StopThreadVolatile implements Runnable {
+	// volatile keyword is used to indicate that a variable's value may be
+	// changed by multiple threads simultaneously.
+	// 用 volatile 修饰的变量，表示不启用线程缓存，每次访问内存获取数据。换句话说，每个
+	// 线程看到的变量值都是一样的，且为任意一个线程最后写入的值。
+	private volatile boolean canceled = false;
+	public void setCanceled(boolean state) { this.canceled = state; }
+	@Override
+	public void run() {
+		int num = 0;
+		try {
+			while (num <= 100000 && !canceled) {
+				if (num % 100 == 0) {
+					System.out.println(num + "是100的倍数。");
+				}
+				num++;
+				Thread.sleep(1);
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		StopThreadVolatile r = new StopThreadVolatile();
+		Thread t = new Thread(r);
+		t.start();
+		Thread.sleep(1000);
+		// 对于主线程修改的值，子线程立即可见
+		r.setCanceled(true);
+	}
+}
+
+
